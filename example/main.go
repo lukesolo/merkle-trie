639a33c2e78a3ca2385b1ed806be99c6ba548c86
lukesolo <suddenmadness@yandex.ru>
2018-05-29 16:37:28 +0300

    Calc tree hash

--- example/main.go
+++ example/main.go
@@ -1,22 +1,40 @@
 package main
 
 import (
+	"crypto/rand"
+	"fmt"
+
 	"github.com/lukesolo/trie"
 )
 
 func main() {
+	depth()
+
 	tree := trie.NewMerkleTrie()
 
 	b1 := []byte{1 << 7}
 	b2 := []byte{1 << 6}
 	b3 := []byte{(1 << 7) + (1 << 6)}
-	tree.Add(b1)
-	tree.Add(b2)
-	tree.Add(b3)
-	tree.Add([]byte{1, 5})
-	tree.Add([]byte{1, 4})
-	tree.Add([]byte{0, 0})
-	tree.Add([]byte{255})
+	tree.Add(b1, b1)
+	tree.Add(b2, b2)
+	tree.Add(b3, b3)
+	tree.Add([]byte{1, 5}, nil)
+	tree.Add([]byte{1, 4}, nil)
+	tree.Add([]byte{0, 0}, nil)
+	tree.Add([]byte{255}, nil)
 
 	tree.Print()
+	fmt.Printf("%x", tree.Hash())
+}
+
+func depth() {
+	tree := trie.NewMerkleTrie()
+
+	for i := 0; i < 1000000; i++ {
+		key := make([]byte, 32)
+		rand.Read(key)
+		tree.Add(key, key)
+	}
+
+	fmt.Println(tree.MaxDepth())
 }
--- trie.go
+++ trie.go
@@ -2,13 +2,14 @@ package trie
 
 import (
 	"bytes"
+	"crypto/sha256"
 	"fmt"
 	"strings"
 )
 
 func NewMerkleTrie() *MerkleTrie {
 	return &MerkleTrie{
-		root:  newNode(0, nil),
+		root:  newNode(0, nil, nil),
 		empty: true,
 	}
 }
@@ -18,24 +19,80 @@ type MerkleTrie struct {
 	empty bool
 }
 
-func (t *MerkleTrie) Add(hash []byte) {
+func (t *MerkleTrie) Add(key, value []byte) {
 	if t.empty {
-		t.root.value = &hash
+		t.root.key = key
+		t.root.value = value
 		t.empty = false
 	} else {
-		t.root.add(hash)
+		t.root.add(key, value)
 	}
+}
+
+func (t *MerkleTrie) Hash() []byte {
+	if t.empty {
+		h := sha256.New()
+		return h.Sum(nil)
+	}
+	return hash(t.root)
+}
+
+func (t *MerkleTrie) MaxDepth() byte {
+	if t.empty {
+		return 0
+	}
+
+	var r func(n *node) byte
+	r = func(n *node) byte {
+		if n.key != nil {
+			return n.level
+		}
+
+		var left, right byte
+		if n.left != nil {
+			left = r(n.left)
+		}
+		if n.right != nil {
+			right = r(n.right)
+		}
+		if left > right {
+			return left
+		}
+		return right
+	}
+	return r(t.root)
+}
+
+func hash(n *node) []byte {
+	if n.key != nil {
+		h := sha256.New()
+		return h.Sum(n.value)
+	}
+	if n.left == nil {
+		return hash(n.right)
+	}
+	if n.right == nil {
+		return hash(n.left)
+	}
+
+	left := hash(n.left)
+	right := hash(n.right)
+	h := sha256.New()
+	h.Write(left)
+	h.Write(right)
+	return h.Sum(nil)
 }
 
 func (t *MerkleTrie) Print() {
 	traversePrint(t.root, "")
 }
 
-func newNode(level byte, value *[]byte) *node {
+func newNode(level byte, key, value []byte) *node {
 	return &node{
 		level:  level,
 		number: level / 8,
 		bit:    byte(1 << (7 - (level % 8))),
+		key:    key,
 		value:  value,
 	}
 }
@@ -43,43 +100,45 @@ func newNode(level byte, value *[]byte) *node {
 type node struct {
 	level, bit, number byte
 	left, right        *node
-	value              *[]byte
+	key, value         []byte
 }
 
-func (n *node) add(hash []byte) {
-	if n.value != nil && bytes.Equal(*n.value, hash) {
+func (n *node) add(key, value []byte) {
+	if n.key != nil && bytes.Equal(n.key, key) {
+		n.value = value
 		return
 	}
 
 	var left bool
-	if n.bit&hash[n.number] == 0 {
+	if n.bit&key[n.number] == 0 {
 		left = true
 	}
 
 	if left {
 		if n.left == nil {
-			n.left = newNode(n.level+1, &hash)
+			n.left = newNode(n.level+1, key, value)
 		} else {
-			n.left.add(hash)
+			n.left.add(key, value)
 		}
 	} else {
 		if n.right == nil {
-			n.right = newNode(n.level+1, &hash)
+			n.right = newNode(n.level+1, key, value)
 		} else {
-			n.right.add(hash)
+			n.right.add(key, value)
 		}
 	}
 
-	if n.value != nil {
-		value := *n.value
+	if n.key != nil {
+		key, value := n.key, n.value
+		n.key = nil
 		n.value = nil
-		n.add(value)
+		n.add(key, value)
 	}
 }
 
 func traversePrint(n *node, prefix string) {
-	if n.value != nil {
-		fmt.Printf("%s %s\n", prefix, formatBinary(*n.value))
+	if n.key != nil {
+		fmt.Printf("%s %s\n", prefix, formatBinary(n.key))
 		return
 	}
 	if n.left != nil {
